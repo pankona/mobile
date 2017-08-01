1566ba781eb14fc678491a009beebb1caf87a380
pankona <yosuke.akatsuka@gmail.com>
2017-08-01 14:36:14 +0900

    change screen size to 1280x720 for osx

--- app/darwin_amd64.m
+++ app/darwin_amd64.m
@@ -205,7 +205,7 @@ runApp(void) {
 	[menu addItem:quitMenuItem];
 	[menuItem setSubmenu:menu];
 
-	NSRect rect = NSMakeRect(0, 0, 600, 800);
+	NSRect rect = NSMakeRect(0, 0, 1280, 720);
 
 	NSWindow* window = [[[NSWindow alloc] initWithContentRect:rect
 			styleMask:NSTitledWindowMask
